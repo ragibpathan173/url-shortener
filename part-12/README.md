6175a6e67e45f65dd9e0070be58767b70266a0e5
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-03-30 10:46:31 +0530

    Add part-12

---
+++ part-12/README.md
@@ -0,0 +1,4 @@
+# Spring Boot URL Shortener - Part 12
+
+* Dockerizing Spring Boot Application
+* Run URL Shortener using Docker Compose
---
+++ part-12/src/main/java/com/sivalabs/urlshortener/domain/services/UserService.java
@@ -0,0 +1,37 @@
+package com.sivalabs.urlshortener.domain.services;
+
+import com.sivalabs.urlshortener.domain.entities.User;
+import com.sivalabs.urlshortener.domain.models.CreateUserCmd;
+import com.sivalabs.urlshortener.domain.models.Role;
+import com.sivalabs.urlshortener.domain.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void createUser(CreateUserCmd cmd) {
+        if (userRepository.existsByEmail(cmd.email())) {
+            throw new RuntimeException("Email already exists");
+        }
+        var user = new User();
+        user.setEmail(cmd.email());
+        user.setPassword(passwordEncoder.encode(cmd.password()));
+        user.setName(cmd.name());
+        user.setRole(cmd.role());
+        user.setCreatedAt(Instant.now());
+        userRepository.save(user);
+    }
+}
