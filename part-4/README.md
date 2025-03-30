59f7ddcc8e821958cf6ce7842b7692f0eb09dabc
ragib <ragibpathan173@gmail.com>
2025-03-30 10:19:53 +0530

    Add part-4

---
+++ part-4/README.md
@@ -0,0 +1,8 @@
+# Spring Boot URL Shortener - Part 4
+
+* Handle LazyLoadingException
+* Understanding OSIV Filter
+* Loading Lazy associations
+    * JPQL
+    * Entity Graph
+* JPA Best Practices
---
+++ part-4/src/main/java/com/sivalabs/urlshortener/domain/services/ShortUrlService.java
@@ -0,0 +1,26 @@
+package com.sivalabs.urlshortener.domain.services;
+
+import com.sivalabs.urlshortener.domain.entities.ShortUrl;
+import com.sivalabs.urlshortener.domain.models.ShortUrlDto;
+import com.sivalabs.urlshortener.domain.repositories.ShortUrlRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShortUrlService {
+
+    private final ShortUrlRepository shortUrlRepository;
+    private final EntityMapper entityMapper;
+
+    public ShortUrlService(ShortUrlRepository shortUrlRepository, EntityMapper entityMapper) {
+            this.shortUrlRepository = shortUrlRepository;
+            this.entityMapper = entityMapper;
+    }
+
+    public List<ShortUrlDto> findAllPublicShortUrls() {
+        return shortUrlRepository.findPublicShortUrls()
+                .stream().map(entityMapper::toShortUrlDto).toList();
+    }
+}
+
