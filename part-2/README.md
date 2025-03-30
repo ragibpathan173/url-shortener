1955817b4d0756781c43897f77e0110535beccef
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-03-30 10:19:53 +0530

    Add part-4

---
+++ part-2/README.md
@@ -0,0 +1,10 @@
+# Spring Boot URL Shortener - Part 2
+
+* Create Spring Boot Project
+* Understand Spring Boot project structure
+* Learn how to handle Web Requests
+* Difference between @Controller and @RestController
+* Render HTML page
+* Using Thymeleaf to render HTML Templates with Dynamic Data
+* Create Layout Template
+* Using WebJars for loading static resources
--- part-3/src/main/java/com/sivalabs/urlshortener/domain/services/ShortUrlService.java
+++ part-3/src/main/java/com/sivalabs/urlshortener/domain/services/ShortUrlService.java
@@ -1,7 +1,6 @@
 package com.sivalabs.urlshortener.domain.services;
 
 import com.sivalabs.urlshortener.domain.entities.ShortUrl;
-import com.sivalabs.urlshortener.domain.models.ShortUrlDto;
 import com.sivalabs.urlshortener.domain.repositories.ShortUrlRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +8,13 @@ import java.util.List;
 
 @Service
 public class ShortUrlService {
-
     private final ShortUrlRepository shortUrlRepository;
-    private final EntityMapper entityMapper;
 
-    public ShortUrlService(ShortUrlRepository shortUrlRepository, EntityMapper entityMapper) {
+    public ShortUrlService(ShortUrlRepository shortUrlRepository) {
         this.shortUrlRepository = shortUrlRepository;
-        this.entityMapper = entityMapper;
     }
 
-    public List<ShortUrlDto> findAllPublicShortUrls() {
-        return shortUrlRepository.findPublicShortUrls()
-                .stream().map(entityMapper::toShortUrlDto).toList();
+    public List<ShortUrl> findAllPublicShortUrls() {
+        return shortUrlRepository.findPublicShortUrls();
     }
 }
